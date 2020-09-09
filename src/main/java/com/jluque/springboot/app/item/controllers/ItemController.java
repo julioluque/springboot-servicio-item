7e5280b21a9059b11a7429a59e179c461fec9025
julioluque <julio.a.luque@gmail.com>
2020-09-09 04:41:52 -0300

    Cliente RestTemplate OK / Feign OK -> dejando estable feign

--- src/main/java/com/jluque/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/jluque/springboot/app/item/controllers/ItemController.java
@@ -15,7 +15,7 @@ import com.jluque.springboot.app.item.service.ItemService;
 public class ItemController {
 
 	@Autowired
-	@Qualifier("restTemplate")
+	@Qualifier("serviceFeign")
 	private ItemService itemService;
 
 	@GetMapping("/listar")
