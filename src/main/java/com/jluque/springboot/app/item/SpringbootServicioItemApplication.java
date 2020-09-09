f4de64501fe10086dd5ec2336db981187e9e5c67
julioluque <julio.a.luque@gmail.com>
2020-09-09 06:06:48 -0300

    Se reemplaza ribbon por eureka, Dependencia, starter y properties

--- src/main/java/com/jluque/springboot/app/item/SpringbootServicioItemApplication.java
+++ src/main/java/com/jluque/springboot/app/item/SpringbootServicioItemApplication.java
@@ -2,12 +2,13 @@ package com.jluque.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@RibbonClient("servicio-productos")
+//@RibbonClient("servicio-productos")
+@EnableEurekaClient
 public class SpringbootServicioItemApplication {
 
 	public static void main(String[] args) {
