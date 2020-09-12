433aa61bbb86338b9c04455c1fafdd3c8c344dd7
julioluque <julio.a.luque@gmail.com>
2020-09-12 03:26:09 -0300

    Probando CircuitBreaker

--- src/main/java/com/jluque/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/jluque/springboot/app/item/controllers/ItemController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jluque.springboot.app.item.model.Item;
+import com.jluque.springboot.app.item.model.Producto;
 import com.jluque.springboot.app.item.service.ItemService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class ItemController {
@@ -23,8 +25,22 @@ public class ItemController {
 		return itemService.findAll();
 	}
 
+	@HystrixCommand(fallbackMethod = "metodoAlternativo")
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return itemService.finById(id, cantidad);
+	}
+
+	public Item metodoAlternativo(Long id, Integer cantidad) {
+		Item item = new Item();
+		Producto producto = new Producto();
+
+		item.setCantidad(cantidad);
+		producto.setId(id);
+		producto.setNombre("Camara Sony");
+		producto.setPrecio(500.00);
+		item.setProducto(producto);
+
+		return item;
 	}
 }
