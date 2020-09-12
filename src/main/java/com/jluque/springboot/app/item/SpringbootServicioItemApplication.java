479bb7d44ecdf99c0e5100e08b59ea67576c2a52
julioluque <julio.a.luque@gmail.com>
2020-09-12 03:12:58 -0300

    Circuit Breaker Hystrix -> Dependencia y starter

--- src/main/java/com/jluque/springboot/app/item/SpringbootServicioItemApplication.java
+++ src/main/java/com/jluque/springboot/app/item/SpringbootServicioItemApplication.java
@@ -2,13 +2,14 @@ package com.jluque.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-//@RibbonClient("servicio-productos")
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class SpringbootServicioItemApplication {
 
 	public static void main(String[] args) {
