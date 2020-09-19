13822cc181cb6a671b52c9ba721a23d3066464cc
julio.a.luque <julio.a.luque@gmail.com>
2020-09-19 07:34:54 -0300

    prueba de integracion Config, Zuul Item -> Leyendo atributos de config con nuevo endpoint

--- src/main/java/com/jluque/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/jluque/springboot/app/item/controllers/ItemController.java
@@ -1,9 +1,16 @@
 package com.jluque.springboot.app.item.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +23,14 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 @RestController
 public class ItemController {
 
+	private static final Logger log = LoggerFactory.getLogger(ItemController.class);
+
 	@Autowired
 	@Qualifier("serviceFeign")
 	private ItemService itemService;
+
+	@Value("${configuracion.texto}")
+	private String texto;
 
 	@GetMapping("/listar")
 	public List<Item> listar() {
@@ -32,7 +44,7 @@ public class ItemController {
 	}
 
 	public Item metodoAlternativo(Long id, Integer cantidad) {
-		
+
 		Item item = new Item();
 		Producto producto = new Producto();
 
@@ -44,4 +56,17 @@ public class ItemController {
 
 		return item;
 	}
+
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto) {
+
+		log.info(texto);
+		log.info("puerto: " + puerto);
+		Map<String, String> json = new HashMap<>();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
+	}
+
 }
